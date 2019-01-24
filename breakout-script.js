0bb2b9390de3c2b0e93aa026b9f1d083fd3acf32
KhallilB <khallil.bailey@gmail.com>
2019-01-24 09:22:20 -0800

    paddle control

--- breakout-script.js
+++ breakout-script.js
@@ -1,33 +1,70 @@
-const canvas = document.getElementById("myCanvas");
-const myCanvas = canvas.getContext("2d");
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var ballRadius = 10;
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width - paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
 
-//GAme Box
-myCanvas.beginPath();
-myCanvas.rect(20, 40, 50, 50);
-myCanvas.fillStyle = "#FF0000";
-myCanvas.fill();
-myCanvas.closePath();
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 
-myCanvas.beginPath();
-myCanvas.arc(240, 160, 20, 0, Math.PI * 2, false);
-myCanvas.fillStyle = "blue";
-myCanvas.fill();
-myCanvas.closePath();
+function keyDownHandler(e) {
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = true;
+  } else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = true;
+  }
+}
 
-myCanvas.beginPath();
-myCanvas.rect(160, 10, 100, 40);
-myCanvas.strokeStyle = "rgba(0, 0, 255, 0.5)";
-myCanvas.stroke();
-myCanvas.closePath();
+function keyUpHandler(e) {
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = false;
+  } else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = false;
+  }
+}
 
-const x = canvas.width / 2;
-const y = canvas.height - 30;
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
 
 function draw() {
-  myCanvas.beginPath();
-  myCanvas.arc(50, 50, 10, 0, Math.PI * 2);
-  myCanvas.fillStyle = "#0095DD";
-  myCanvas.fill();
-  myCanvas.closePath();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  drawPaddle();
+
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    dy = -dy;
+  }
+
+  if (rightPressed && paddleX < canvas.width - paddleWidth) {
+    paddleX += 7;
+  } else if (leftPressed && paddleX > 0) {
+    paddleX -= 7;
+  }
+
+  x += dx;
+  y += dy;
 }
+
 setInterval(draw, 10);
